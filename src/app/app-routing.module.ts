6db27a43741cb356dd282c671a183a1dd90db333
Willian Maruno <jackmaruno@gmail.com>
2019-07-31 14:45:40 -0300

    inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -76,7 +76,7 @@ const routes: Routes = [
   },
 ];
 @NgModule({
-    imports: [RouterModule.forRoot(routes, {useHash: false, enableTracing: false})],
+    imports: [RouterModule.forRoot(routes, {useHash: true, enableTracing: false})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
